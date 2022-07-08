9982d547eecbb26291d6f37386d62522f220ee29
park eun hui <7875hi@naver.com>
2022-07-08 15:59:51 +0900

    계산기

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+package example;
+
+public class Calculator {
+
+	int getAdd(int a, int b) {
+		return a+b;
+	}
+	int getSub(int a, int b) {
+		return a-b;
+	}
+	int getMul(int a, int b) {
+		return a*b;
+	}
+	float getDiv(int a, int b) {
+		return (float)a/b;
+	}
+
+}
