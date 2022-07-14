e9f63d1f9b1a1c1f1af56f1e808519eaa0f9ce8b
park eun hui <7875hi@naver.com>
2022-07-14 17:11:48 +0900

    22.07.14 캡슐화 패키지 틀릴때

---
+++ ProtectedvsDefault2.java
@@ -0,0 +1,21 @@
+package AccessModifier_default;
+
+public class ProtectedvsDefault {
+	// 접근 제어자가 protected인 변수
+	protected int Protected;
+	
+	// 접근 제어자가 default인 변수
+	int Default;
+	
+	// 접근 제어자가 protected인 메서드
+	protected void proadd() {
+		
+	}
+	
+	// 접근 제어자가 default인 메서드
+	void defadd() {
+		
+	}	
+	
+
+}
---
+++ PublicvsDefault2.java
@@ -0,0 +1,19 @@
+package AccessModifier_default;
+
+public class PublicvsDefault {
+	// 접근 제어자가 public인 변수
+	public int Public;
+	
+	// 접근 제어자가 default인 변수
+	int Default;
+	
+	// 접근 제어자가 public인 메서드
+	public void pubadd() {
+		
+	}
+	
+	// 접근 제어자가 default인 메서드
+	void defadd() {
+		
+	}
+}
